2a4951bb0648799dfde099ffe750880531866089
Anushil Raman <anushilraman1993@gmail.com>
2023-11-07 02:12:46 +0530

    Injecting the Identity services and creating new database for authenticating the user

---
+++ NZWalks.API/Data/NzWalksAuthDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NZWalks.API.Data
+{
+    public class NzWalksAuthDbContext : IdentityDbContext
+    {
+        public NzWalksAuthDbContext(DbContextOptions<NzWalksAuthDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            var readerRoleId = "e162c4e6-17bb-4e17-965d-13aae5335d81";
+            var writerRoleId = "236b9b95-01be-4bd7-92dd-e08f337fb6e2";
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id= readerRoleId,
+                    ConcurrencyStamp=readerRoleId,
+                    Name ="Reader",
+                    NormalizedName = "READER",
+                },
+                new IdentityRole
+                {
+                    Id= writerRoleId,
+                    ConcurrencyStamp=writerRoleId,
+                    Name ="Writer",
+                    NormalizedName = "WRITER",
+                }
+            };
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
+    }
+}
--- NZWalks.API/Program.cs
+++ NZWalks.API/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using NZWalks.API.Data;
@@ -14,8 +15,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<NZWalksDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("NzWalksConnectionstring")));
+
+builder.Services.AddDbContext<NzWalksAuthDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("NzWalksAuthConnectionstring")));
 
 //registering repository (class and interface) in the application
 // Scoped is used for whole life spain of the app
@@ -23,6 +28,22 @@ builder.Services.AddScoped<IRegionRepository, RegionRepository>();
 builder.Services.AddScoped<IWalkRepository, WalksRepository>();
 
 builder.Services.AddAutoMapper(typeof(AutoMapperProfiles));
+
+builder.Services.AddIdentityCore<IdentityUser>()
+    .AddRoles<IdentityRole>()
+    .AddTokenProvider<DataProtectorTokenProvider<IdentityUser>>("NZWalks")
+    .AddEntityFrameworkStores<NzWalksAuthDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+});
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
 options.TokenValidationParameters = new TokenValidationParameters
