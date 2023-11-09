ea1f23aa30f97e04ce2dc6c36d782f4cdc24e5af
Anushil Raman <anushilraman1993@gmail.com>
2023-11-10 00:58:06 +0530

    Consuming Web Api from CLient Side

---
+++ NZWlaks.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NZWlaks.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
